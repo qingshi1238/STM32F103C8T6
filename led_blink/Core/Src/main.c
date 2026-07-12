aac8eba86b10d0bd87cc6067d929ae830748dd5c
qingshi1238 <3072126418@qq.com>
2026-07-12 12:11:01 +0800

    led_blink

--- led_blink/Core/Src/main.c
+++ led_blink/Core/Src/main.c
@@ -98,9 +98,9 @@ int main(void)
   while (1)
   {
     HAL_GPIO_WritePin(GPIOC,GPIO_PIN_13, GPIO_PIN_RESET);
-    HAL_Delay(1000);
+    HAL_Delay(500);
     HAL_GPIO_WritePin(GPIOC,GPIO_PIN_13, GPIO_PIN_SET);
-    HAL_Delay(1000);
+    HAL_Delay(500);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
